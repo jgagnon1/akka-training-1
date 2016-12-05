71ea68d5a89c46eb4ae0dc8d1492085371287737
Shilei Long <shilei@paytm.com>
2016-12-05 13:47:08 -0500

    RequestProxy

--- src/main/scala/com/example/RequestProxy.scala
+++ src/main/scala/com/example/RequestProxy.scala
@@ -1,16 +1,32 @@
 package com.example
 
-import akka.actor.{Actor, Props}
+import akka.actor.{Actor, ActorRef, Props, Terminated}
 import akka.actor.Actor.Receive
+
+import scala.collection.mutable
 
 /**
   * Created by jerome on 2016-12-05.
   */
 class RequestProxy extends Actor {
 
+  var userSessionActors = Map.empty[Long, ActorRef]
+
   override def receive: Receive = {
-    case request: Request =>
-      println(s"Received request: $request")
+    case r@Request(sessionId, timestamp, _, _, _) =>
+      val userSessionActor = userSessionActors.getOrElse(sessionId, {
+        val sessionActor = context.actorOf(SessionActor.props())
+        userSessionActors += (sessionId -> sessionActor)
+        context.watch(userSessionActor)
+        sessionActor
+      })
+      userSessionActor ! r
+
+    case Terminated(terminatedActor) =>
+      // FIXME : Optimize find with BiMap
+      userSessionActors
+        .find { case ((_, ref)) => ref == terminatedActor }
+        .foreach { case (sessionId, _) => userSessionActors -= sessionId }
   }
 
 }
@@ -20,3 +36,4 @@ object RequestProxy {
   def props() = Props[RequestProxy]
 
 }
+
